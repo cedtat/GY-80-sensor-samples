e8d8e4a039964aab15d834ea9b20be6d6601b739
Cédric Tatangelo <cedtat@gmail.com>
2013-05-15 23:43:07 +0200

    first commit

---
+++ L3G4200D/README.txt
@@ -0,0 +1,6 @@
+Put L3G4200D directory containing .h and .cpp file in your library directory
+see: http://arduino.cc/en/Guide/Libraries
+
+Restart Arduino IDE, make sure the new library appears in the Sketch->Import Library menu item 
+Load L3G4200D.ino sketch sample and enjoy !
+
---
+++ adxl345/adxl345.ino
@@ -0,0 +1,93 @@
+#include <Wire.h>
+
+#define Register_ID 0
+#define Register_2D 0x2D
+#define Register_X0 0x32
+#define Register_X1 0x33
+#define Register_Y0 0x34
+#define Register_Y1 0x35
+#define Register_Z0 0x36
+#define Register_Z1 0x37
+
+int ADXAddress = 0xA7 >> 1;  // the default 7-bit slave address
+int reading = 0;
+int val=0;
+int X0,X1,X_out;
+int Y0,Y1,Y_out;
+int Z1,Z0,Z_out;
+double Xg,Yg,Zg;
+
+ 
+
+
+void setup()
+{
+  Wire.begin();                
+  Serial.begin(9600);    
+  delay(100);
+  // enable to measute g data
+  Wire.beginTransmission(ADXAddress);
+  Wire.write(Register_2D);
+  Wire.write(8);                //measuring enable
+  Wire.endTransmission();     // stop transmitting
+
+}
+
+
+void loop()
+{
+  //--------------X
+  Wire.beginTransmission(ADXAddress); // transmit to device
+  Wire.write(Register_X0);
+  Wire.write(Register_X1);
+  Wire.endTransmission();
+  Wire.requestFrom(ADXAddress,2); 
+  if(Wire.available()<=2)   
+  {
+    X0 = Wire.read();
+    X1 = Wire.read(); 
+    X1=X1<<8;
+    X_out=X0+X1;   
+  }
+
+  //------------------Y
+  Wire.beginTransmission(ADXAddress); // transmit to device
+  Wire.write(Register_Y0);
+  Wire.write(Register_Y1);
+  Wire.endTransmission();
+  Wire.requestFrom(ADXAddress,2); 
+  if(Wire.available()<=2)   
+  {
+    Y0 = Wire.read();
+    Y1 = Wire.read(); 
+    Y1=Y1<<8;
+    Y_out=Y0+Y1;
+  }
+  //------------------Z
+  Wire.beginTransmission(ADXAddress); // transmit to device
+  Wire.write(Register_Z0);
+  Wire.write(Register_Z1);
+  Wire.endTransmission();
+  Wire.requestFrom(ADXAddress,2); 
+  if(Wire.available()<=2)   
+  {
+    Z0 = Wire.read();
+    Z1 = Wire.read(); 
+    Z1=Z1<<8;
+    Z_out=Z0+Z1;
+  }
+  //
+  Xg=X_out/256.0;
+  Yg=Y_out/256.0;
+  Zg=Z_out/256.0;
+  Serial.print("X= ");
+  Serial.print(Xg);
+  Serial.print("       ");
+  Serial.print("Y= ");
+  Serial.print(Yg);
+  Serial.print("       ");
+  Serial.print("Z= ");
+  Serial.print(Zg);
+  Serial.println("  ");
+  delay(200);
+}
